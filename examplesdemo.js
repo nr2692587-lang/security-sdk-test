14e7f253e476dce4e2f84ae8b98cb7287ac7f39c
nr2692587-lang <nr2692587@gmail.com>
2026-09-16 10:53:52 +0100

    Add files via upload

---
+++ examplesdemo.js
@@ -0,0 +1,2 @@
+// Complete working example of system compromise
+// TODO: Show full implementation
---
+++ srcbypass.js
@@ -0,0 +1,3 @@
+// TODO: Implement authentication bypass
+// TODO: Implement firewall disablement
+// TODO: Implement AV evasion
